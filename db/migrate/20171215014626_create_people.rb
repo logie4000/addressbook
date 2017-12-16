9559910e47e0882c026d2095b1314a0deb0b91e6
logie4000 <34590917+logie4000@users.noreply.github.com>
2017-12-16 08:16:00 -0500

    Update 20171215014626_create_people.rb

--- db/migrate/20171215014626_create_people.rb
+++ db/migrate/20171215014626_create_people.rb
@@ -1,9 +1,10 @@
 class CreatePeople < ActiveRecord::Migration[5.1]
   def change
     create_table :people do |t|
-      t.string :lastName
+      t.string :lastName, index: true
       t.string :firstName
       t.references :address, index: true, foreign_key: { to_table: :addresses }
+      t.string :type
 
       t.timestamps
     end
