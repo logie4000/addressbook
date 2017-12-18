9265dc39d56232847647d60885d7cc1eade61506
logie4000 <34590917+logie4000@users.noreply.github.com>
2017-12-18 17:21:04 -0500

    Update addresses_helper.rb

--- app/helpers/addresses_helper.rb
+++ app/helpers/addresses_helper.rb
@@ -1,13 +1,3 @@
 module AddressesHelper
-  def setup_address(address)
-    if (address.spouse.nil?)
-      address.build_spouse()
-    end
-    
-    if (address.children.empty?)
-      address.children.build()
-    end
-    
-    return address
-  end
+
 end
