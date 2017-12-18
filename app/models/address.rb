039b76cf65608931e366192fff43c589ff711b4a
logie4000 <34590917+logie4000@users.noreply.github.com>
2017-12-18 09:52:30 -0500

    Update address.rb

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,4 @@
 class Address < ApplicationRecord
   has_many :children, :dependent => :destroy
-  has_one :spouse, :class_name => "Person", :optional => true
+  belongs_to :spouse, :class_name => "Person", :optional => true
 end
