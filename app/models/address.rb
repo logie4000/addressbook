7bc66927677f74bd66b09b9823dd67a7e933153e
logie4000 <34590917+logie4000@users.noreply.github.com>
2017-12-18 17:18:04 -0500

    Update address.rb

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,6 @@
 class Address < ApplicationRecord
   has_many :children, :class_name => "Person", :dependent => :destroy
-  belongs_to :spouse, :class_name => "Person", :dependent => :destroy
+  has_one :spouse, :class_name => "Person", :dependent => :destroy
   
   accepts_nested_attributes_for :spouse, :allow_destroy => true, reject_if: :all_blank
   accepts_nested_attributes_for :children, :allow_destroy => true, reject_if: :all_blank
