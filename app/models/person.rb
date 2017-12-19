48c8b2e5ce65b0909efd24715f0f7dadd0148660
logie4000 <34590917+logie4000@users.noreply.github.com>
2017-12-19 15:16:32 -0500

    Update person.rb

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,6 @@
 class Person < ApplicationRecord
   belongs_to :address
+  
+  has_many :children, :class_name => "Person", :foreign_key => "parent_id"
+  belongs_to :parent, :class_name => "Person"
 end
