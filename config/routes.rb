2d18a4e2871f9a1e32d0b52ca7284d3b93f6ef9b
logie4000 <34590917+logie4000@users.noreply.github.com>
2017-12-20 15:11:55 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   resources :addresses
-  resources :people
+  resources :people do
+    collection do
+      get :add_child_row
+    end
+  
+    member do
+      patch :add_child
+      patch :remove_child
+    end
+  end
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
