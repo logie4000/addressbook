4baf19633285eb51f02798008ca0f3987580ec31
logie4000 <34590917+logie4000@users.noreply.github.com>
2017-12-20 08:34:11 -0500

    Update person.rb

--- app/models/person.rb
+++ app/models/person.rb
@@ -4,4 +4,6 @@ class Person < ApplicationRecord
   belongs_to :spouse, :class_name => "Person", :optional => true
   has_many :children, :class_name => "Person", :foreign_key => "parent_id"
   belongs_to :parent, :class_name => "Person", :optional => true
+  
+  accepts_nested_attributes_for :address, :allow_destroy => true, reject_if: :all_blank
 end
