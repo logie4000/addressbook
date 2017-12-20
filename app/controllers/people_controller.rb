7b0e774e035c4aa9eca4f6e6eddcfa364d7fa026
logie4000 <34590917+logie4000@users.noreply.github.com>
2017-12-20 14:13:53 -0500

    Update people_controller.rb

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -83,8 +83,10 @@ class PeopleController < ApplicationController
       @person = Person.find(params[:id])
       if (@person.parent)
         @person = @person.parent
-      elsif (@person.children.empty? && @person.spouse && !@person.spouse.children.empty?)
-        @person = @person.spouse
+      end
+      
+      if (@person != @person.address.resident)
+        @person = @person.address.resident
       end
     end
 
