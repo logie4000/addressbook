2e291e9b457e7cbc62bd0560ce860feed61c8c45
Logie4000 <logie4000@gmail.com>
2024-01-15 18:53:12 +0000

    Sync live with repo

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ gem 'rails', '~> 5.1.4'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use mysql2
-gem 'mysql2'
+gem 'mysql2', '~> 0.4.9'
 
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171215014626) do
+ActiveRecord::Schema.define(version: 20171223032220) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "lastName"
-    t.string "firstName"
     t.string "street1"
     t.string "street2"
     t.string "city"
@@ -23,21 +21,26 @@ ActiveRecord::Schema.define(version: 20171215014626) do
     t.text "comments"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "spouse_id"
-    t.index ["spouse_id"], name: "index_addresses_on_spouse_id"
+    t.bigint "addressee_id"
+    t.string "lastSent"
+    t.index ["addressee_id"], name: "index_addresses_on_addressee_id"
   end
 
   create_table "people", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "lastName"
     t.string "firstName"
     t.bigint "address_id"
+    t.bigint "parent_id"
+    t.bigint "spouse_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "suffix"
     t.index ["address_id"], name: "index_people_on_address_id"
     t.index ["lastName"], name: "index_people_on_lastName"
+    t.index ["parent_id"], name: "index_people_on_parent_id"
+    t.index ["spouse_id"], name: "index_people_on_spouse_id"
   end
 
-  add_foreign_key "addresses", "people", column: "spouse_id"
+  add_foreign_key "addresses", "people", column: "addressee_id"
   add_foreign_key "people", "addresses"
 end
-
